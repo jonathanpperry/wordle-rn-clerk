8f286f1e050b95d88721f92da908aeaa1623eced
Jonathan Perry <jpper@umich.edu>
2024-10-31 18:28:59 -0400

    Add support for svgs (react-native-svg)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "@/assets/images/wordle-icon.svg";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Icon width={70} height={70} />
+        {/* <Text style={styles.title}>Worldle</Text>
+        <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text> */}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 50,
+    gap: 40,
+  },
+  header: {
+    alignItems: "center",
+    gap: 10,
+  },
+  title: {
+    fontSize: 40,
+  },
+  text: {
+    fontSize: 26,
+    textAlign: "center",
+  },
+  footer: {},
+});
