7a6bfa65d93a7038b775455f62e2274fa49e31d5
Jon Perry <jonperry113@gmail.com>
2026-05-17 00:24:24 -0400

    Built wordle game ui

--- app/game.tsx
+++ app/game.tsx
@@ -1,3 +1,6 @@
+import OnScreenKeyboard from "@/components/OnScreenKeyboard";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 import { Stack, useRouter } from "expo-router";
 import { useEffect, useRef, useState } from "react";
 import {
@@ -20,15 +23,103 @@ import Animated, {
 
 const ROWS = 6;
 
-
 const Page = () => {
+  const colorScheme = useColorScheme();
+  const scheme = (colorScheme ?? "light") as "light" | "dark";
+
+  const backgroundColor = Colors[scheme].gameBg;
+  const textColor = Colors[scheme].text;
+  const grayColor = Colors[scheme].gray;
+
+  const router = useRouter();
+
+  const [rows, setRows] = useState<string[][]>(
+    new Array(ROWS).fill(new Array(5).fill("a")),
+  );
+
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const [greenLetters, setGreenLetters] = useState<string[]>([]);
+  const [yellowLetters, setYellowLetters] = useState<string[]>([]);
+  const [grayLetters, setGrayLetters] = useState<string[]>([]);
+
+  const addKey = (key: string) => {
+    console.log("addKey", key);
+    // TODO: implement
+  };
+
+  console.log(rows);
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <View style={styles.headerIcon}>
+              <Ionicons
+                name="help-circle-outline"
+                size={28}
+                color={textColor}
+              />
+              <Ionicons name="podium-outline" size={28} color={textColor} />
+              <Ionicons name="settings-sharp" size={28} color={textColor} />
+            </View>
+          ),
+        }}
+      />
+
+      <View style={styles.gameField}>
+        {rows.map((row, rowIndex) => (
+          <View style={styles.gameFieldRow} key={`row-${rowIndex}`}>
+            {row.map((cell, cellIndex) => (
+              <View style={styles.cell} key={`cell-${rowIndex}-${cellIndex}`}>
+                <Text style={styles.cellText}>{cell}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+
+      <OnScreenKeyboard
+        onKeyPressed={addKey}
+        greenLetters={greenLetters}
+        yellowLetters={yellowLetters}
+        grayLetters={grayLetters}
+      />
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 40,
+  },
+  headerIcon: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  gameField: {
+    alignItems: "center",
+    gap: 8,
+  },
+  gameFieldRow: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  cell: {
+    width: 62,
+    height: 62,
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellText: {
+    fontSize: 30,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
